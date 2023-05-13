acc4212a244cb20739280e529cff0fb3c049e7b4
rickysoro <ricky.soro@gmail.com>
2023-05-13 09:55:06 +0200

    Added Models

---
+++ src/BagOfNailsApp.Core/Model/Nail.cs
@@ -0,0 +1,5 @@
+﻿namespace BagOfNailsApp.Core.Model;
+public class Class1
+{
+
+}
---
+++ test/BagOfNails.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BagOfNails.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
